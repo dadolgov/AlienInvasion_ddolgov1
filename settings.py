44a8988f82c678b7d5642cf916d638db83df98e8
Dmitrii Dolgov <ddolgov1@student.cscc.edu>
2026-04-08 22:32:20 -0400

    created a ship class and set up to draw to screen

--- settings.py
+++ settings.py
@@ -6,3 +6,9 @@ class Settings:
         self.screen_h:int=800
         self.fps:int=60
         self.bg_file=Path.cwd()/"Assets"/"images"/"Starbasesnow.png"
+
+        self.ship_file=Path.cwd()/"Assets"/"images"/"ship2(no bg).png"
+        self.ship_w=40
+        self.ship_h=60
+        
+
