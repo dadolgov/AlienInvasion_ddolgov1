38d8df92a4e080683308c6977b09382ba6e046fc
Dmitrii Dolgov <ddolgov1@student.cscc.edu>
2026-04-17 20:29:35 -0400

    added alien settings

--- settings.py
+++ settings.py
@@ -20,8 +20,17 @@ class Settings:
 
         self.bullet_file=Path.cwd()/"Assets"/"images"/"laserBlast.png"
         self.laser_sound=Path.cwd()/"Assets"/"sound"/"laser.mp3"
+        self.impact=Path.cwd()/"Assets"/"sound"/"impactSound.mp3"
+
         self.bullet_speed=7
         self.bullet_w=25
         self.bullet_h=80
         self.bullet_amount=5
+
+        self.alien_file=Path.cwd()/"Assets"/"images"/"enemy_4.png"
+        self.fleet_speed=2
+        self.fleet_drop_speed=40
+        self.alien_w=40
+        self.alien_h=40
+        self.fleet_direction=1
 
